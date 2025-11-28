86a084b8caccc04f79c128e5b3b231405d613f2e
Egor <egork2205@gmail.com>
2025-11-28 22:49:01 +0700

    Задание 12.1.3

--- TasksForModul12/Program.cs
+++ TasksForModul12/Program.cs
@@ -10,6 +10,20 @@ namespace TasksForModul12
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        static void FillArray()
+        {
+            Console.WriteLine("Enter the number of array elements...");
+            int count = Convert.ToInt32(Console.ReadLine());
+            var array = new string [count];
+
+            for (int i = 0; i < count; i++)
+            {
+                array [i] = Console.ReadLine();
+            }
+
         }
     }
 }
